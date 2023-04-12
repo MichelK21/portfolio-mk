000f227b31692f64be8e160ecf94f6f91b859a38
Michel KACOU <Your email>
2023-04-12 21:51:51 +0200

    About section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,34 @@ import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
-  return <div className=" w-full h-screen bg-[#0a192f]">Home</div>;
+  return (
+    <div name="home" className=" w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-white text-[25px]"> Hi, my name is </p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Michel KACOU
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Full Stack Developper
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          {" "}
+          I'm a Full Stack Developper with a background in front-end and
+          back-end programming, I'm capable to build a website or an mobile app
+          from the conception to the deployment.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-600 hover:border-gray-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
